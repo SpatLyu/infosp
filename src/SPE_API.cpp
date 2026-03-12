e2089c2e36c5aaa0607589780205a142fdf56095
SpatLyu <lyu.geosocial@gmail.com>
2026-03-13 00:00:02 +0800

    draft spe utility

--- src/SPE_API.cpp
+++ src/SPE_API.cpp
@@ -33,17 +33,17 @@ double RcppSPE4Lattice(
     }
     const size_t n_vars = v.size();
 
-    std::vector<std::vector<double>> cppMat(n_vars, std::vector<double>(n_obs));
-    for (int r = 0; r < n_obs; ++r) {
-        for (int c = 0; c < n_vars; ++c) {
-            cppMat[c][r] = mat(r, c);
-        }
-    }
-
     std::vector<std::vector<std::vector<uint8_t>>> pm;
     pm.resize(n_vars);
         for (size_t j = 0; j < n_vars; ++j)
             mat[j].reserve(n_obs);
+
+    for (size_t idx : v) {
+        std::vector<double> vec(n_obs);
+        for (size_t r = 0; r < n_obs; ++r) {
+            vec[r] = mat(r, idx);
+        }
+    }
 
 
 }
